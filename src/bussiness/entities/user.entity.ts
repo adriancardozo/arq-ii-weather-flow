ae9d258316c9d169e1ef996e4a62358244cfb93b
Adrián Cardozo <cardozoadrian997@gmail.com>
2026-04-28 14:17:10 -0300

    Add User tests

--- src/bussiness/entities/user.entity.ts
+++ src/bussiness/entities/user.entity.ts
@@ -8,7 +8,8 @@ export class User {
   email: string;
   password: string;
 
-  constructor(firstName: string, lastName: string, email: string, password: string) {
+  constructor(id: string, firstName: string, lastName: string, email: string, password: string) {
+    this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
